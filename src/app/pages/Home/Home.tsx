adc720a17eaef5376a3b636b46daf3fd7adeb0e9
Anthony <aretelew@gmail.com>
2025-02-07 15:11:08 -0500

    Added carousel

--- src/app/pages/Home/Home.tsx
+++ src/app/pages/Home/Home.tsx
@@ -1,11 +1,21 @@
 import { HeaderSimple } from "../../../components/HeaderSimple.tsx";
-import myImage from "../../../assets/images/placeholder.png";
+import { Carousel } from '@mantine/carousel';
+import { Image } from '@mantine/core';
+import myImage from "../../../assets/images/LMF04307.jpg";
+import "@mantine/carousel/styles.css";
+import "./Home.module.css";
 
 export default function Home() {
     return (
         <div className="home-container">
             <HeaderSimple />
-            <img src={myImage} alt="Description" className="home-image" />
+            <div className="carousel-container">
+                <Carousel withIndicators slideSize="50%" loop>
+                    <Carousel.Slide><Image src={myImage} /></Carousel.Slide>
+                    <Carousel.Slide><Image src={myImage} /></Carousel.Slide>
+                    <Carousel.Slide><Image src={myImage} /></Carousel.Slide>
+                </Carousel>
+            </div>
         </div>
     );
 }
