360bd6e6d45a505ab36c4e2f97cd67066afd9255
Anthony <aretelew@gmail.com>
2025-02-10 19:21:55 -0500

    Added carousel

--- src/app/pages/Home/Home.tsx
+++ src/app/pages/Home/Home.tsx
@@ -1,18 +1,32 @@
-import { HeaderSimple } from "../../../components/HeaderSimple.tsx";
+import { useRef } from 'react';
+import { HeaderSimple } from '../../../components/HeaderSimple.tsx';
 import { Carousel } from '@mantine/carousel';
 import { Image } from '@mantine/core';
-import myImage from "../../../assets/images/LMF04307.jpg";
-import "@mantine/carousel/styles.css";
-import "./Home.module.css";
+import Autoplay from 'embla-carousel-autoplay';
+import myImage from '../../../assets/images/butler_bash_car_lineup.jpg';
+import styles from './Home.module.css';
+import '@mantine/carousel/styles.css';
+
 
 export default function Home() {
+    const autoplay = useRef(Autoplay({ delay: 5000 }));
     return (
         <div className="home-container">
             <HeaderSimple />
-            <div className="carousel-container">
-                <Carousel withIndicators slideSize="50%" loop>
-                    <Carousel.Slide><Image src={myImage} /></Carousel.Slide>
-                    <Carousel.Slide><Image src={myImage} /></Carousel.Slide>
+            <div className={styles.carouselContainer}>
+                <Carousel
+                    withIndicators
+                    slideSize="50%"
+                    height="50vh"
+                    draggable={true}
+                    plugins={[autoplay.current]}
+                    onMouseEnter={autoplay.current.stop}
+                    onMouseLeave={autoplay.current.reset}
+                    slideGap="md"
+                    loop
+                >
+                    <Carousel.Slide><Image src={myImage}/></Carousel.Slide>
+                    <Carousel.Slide><Image src={myImage}/></Carousel.Slide>
                     <Carousel.Slide><Image src={myImage} /></Carousel.Slide>
                 </Carousel>
             </div>
